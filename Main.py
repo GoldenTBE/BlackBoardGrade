91f5b9a09e2a5a9e3b5d8e94a54e7c982333c5cb
GoldenTBE <dsyoung0731@gmail.com>
2020-10-04 01:28:08 -0400

    1.02

--- Main.py
+++ Main.py
@@ -5,6 +5,8 @@ PATH = "C:/Program Files (x86)\chromedriver.exe"
 
 driver = webdriver.Chrome(PATH)
 
+'''FIRST TWO FUNCTIONS ARE FOR TESTING IF THE INPUT IS AN INT OR STRING
+-----------------------------------------------------------------------'''
 def is_it_int():
     while True:
         var = input()
@@ -25,6 +27,18 @@ def is_it_str():
             print("Was that a String? Please try again...")
     return var
 
+
+'''------------------------------------------------------------------------------------'''
+'''THESE TWO FUNCTIONS ASKS FOR USERNAME & PASSWORD / LOGGING IN WITH THOSE CREDENTIALS /
+---------------------------------------------------------------------------------------'''
+def prompt_login_info():
+    print("What is your NetID?")
+    username = is_it_str()
+    print("What is your password?")
+    password = is_it_str()
+
+    return [username, password]
+
 def login(user, password):
     driver.get('https://huskyct.uconn.edu/') #goes to huskyCT
     driver.find_element_by_class_name("button-1").click() #gets rid of asking for cookies
@@ -34,14 +48,9 @@ def login(user, password):
     driver.find_element_by_id("button").click()
 
 
-def prompt_login_info():
-    print("What is your NetID?")
-    username = is_it_str()
-    print("What is your password?")
-    password = is_it_str()
 
-    return [username, password]
-
+'''---------------------------'''
+'''Functions that need editing '''
 
 def return_classes():
     driver.maximize_window()
@@ -51,10 +60,21 @@ def return_classes():
     for j, i in enumerate(driver.find_elements_by_tag_name('h4')):
         classes.append(i.get_attribute("Title"))
         print("Class " + str(j + 1) + ": " + i.get_attribute("Title"))
+    for length in classes[-1]:
+        print("-", end = '')
     return classes
 
 
 '''def getting_grades():'''
+
+def Get_Assignmets():
+    driver.find_element_by_xpath("//*[@id=\"base_tools\"]/bb-base-navigation-button[6]/div/li/a/ng-switch/span").click()
+
+
+
+
+
+'''----------------------------------------------------------------------'''
 
 def main():
     print("Grade module has begun. Start time : " + time.ctime())
@@ -65,15 +85,13 @@ def main():
         quit()
     login(username, password)
     return_classes()
-    int(input("\n 1:Returns class grades "))
+    Get_Assignmets()
+    print("\n1:Returns class grades(In-Process)\n2:Drop-File off (not supported yet)\n3:Due Dates for activities")
 
+    print("BlackBoard Module has ended. End Time : " + time.ctime())
+    quit()
 
-
-    print("Grade module has ended. End Time : " + time.ctime())
 
 
 main()
 
-#key takeaways
-'''what to work on: making while, break, except a function. / do grading module for all classes, specific class + 
-assignments, and assignments that are due'''
--- README.md
+++ README.md
@@ -2,6 +2,12 @@ Changelog 1.0.1
 -
 - Added Condition if username or password is not string.
 
+Changelog 1.0.2
+-
+- Starting the Get.Assignment module 
+- Added '-' after returning classes 
+- Next update will change so classes won't be returned unless the user is asked(Rearraning main function)
+
 Roadmap for BlackBoardGrade 
 -
 -Coding the return function for grades
